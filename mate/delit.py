2611337f4899975168d60bd204a58e832a820737
test <test@gmail.com>
2022-12-01 18:38:35 +0200

    fixes

--- mate/delit.py
+++ mate/delit.py
@@ -1,36 +1,14 @@
-from functools import wraps
-
-
-def delay(seconds: int) -> callable:
-    """
-    delay function
-    """
-    def inner(func: callable):
-        """
-        inner function
-        """
-        @wraps(func)
-        def wrapper(*args, **kwargs) -> None:
-            """
-            wrapper function
-            """
-            import time
-            s = time.perf_counter()
-            time.sleep(seconds)
-            func(*args, **kwargs)
-            e = time.perf_counter()
-            print('Elapsed: ', e - s)
-        return wrapper
-    return inner
-
-
-@delay(seconds=3)
-def hello_name(msg: str) -> None:
-    """
-    hello name function
-    """
-    print(f"Hello, {msg}!")
-
-
-hello_name("Nikita")
-
+import csv
+import time
+with open("output_csv_full.csv") as file:
+    csv_reader = csv.reader(file, delimiter=',')
+    c = 0
+    s = time.perf_counter()
+    for row in csv_reader:
+        c += 1
+        if c % 10000 == 0:
+            print(c)
+    print(f'Processed {c} lines.')
+    e = time.perf_counter()
+    print('Elapsed: ', e - s, ' sec.')
+    print(csv_reader.line_num)
