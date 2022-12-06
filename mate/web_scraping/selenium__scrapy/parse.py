ed82de527c3533891afb37d1007f1656d26e02e4
usrofgh <mailforgimail@gmail.com>
2022-12-06 23:09:15 +0200

    selenium notes

--- mate/web_scraping/selenium__scrapy/parse.py
+++ mate/web_scraping/selenium__scrapy/parse.py
@@ -1,22 +1,20 @@
 import csv
+import requests
 import logging
 import os.path
 import sys
-import requests
+
 
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 from dataclasses import dataclass, astuple, fields
 from urllib.parse import urljoin
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-
-# selenium - не для парсинга, а для автоматического тестирования. Но можно и парсить
-#  bs4, scrapy - с их помощью нет возможности нажимать на кнопки
-
+from selenium.webdriver.remote.webdriver import WebDriver
 
 URL_BASE = "https://webscraper.io/"
-LAPTOP_URL = urljoin(URL_BASE, "test-sites/e-commerce/static/computers/laptops")
+LAPTOP_URL = urljoin(URL_BASE, "test-sites/e-commerce/allinone/computers/laptops")
 
 PRODUCTS_OUTPUT_CSV_PATH = "products.csv"
 
@@ -31,41 +29,62 @@ class Product:
     additional_info: dict
 
 
+_driver: WebDriver | None = None
 PRODUCTS_FIELDS = [field.name for field in fields(Product)]
 
+
+def get_driver() -> WebDriver:
+    return _driver
+
+
+def set_driver(new_driver: WebDriver) -> None:
+    global _driver
+    _driver = new_driver
+
+
+
 logging.basicConfig(
-    level=logging.DEBUG,  # debug/info - информирование, есть и warning/error. Debug 0-й уровень, потом по возрастанию
-    # requests тоже в debug выводит инфо. Поэтому если указали дебаг, то помимо моего также будет выводиться с request
-    format="[%(levelname)8s]: %(message)s",  # levelname - занимает 8 символов
-    handlers=[  # то, как будут обрабатываться логи
-        logging.FileHandler(os.path.join("parser.log")),  # записывает  все в parser.log
-        logging.StreamHandler(sys.stdout),  # также будет транслироваться в консоле
+    level=logging.INFO,
+    format="[%(levelname)8s]: %(message)s",
+    handlers=[
+        logging.FileHandler(os.path.join("parser.log")),
+        logging.StreamHandler(sys.stdout),
     ],
 )
 
 
 def write_products_to_csv(products: [Product]) -> None:
-    with open(PRODUCTS_OUTPUT_CSV_PATH, "w") as file:
+    with open(PRODUCTS_OUTPUT_CSV_PATH, "w", newline="") as file:
         writer = csv.writer(file)
         writer.writerow(PRODUCTS_FIELDS)
         writer.writerows([astuple(product) for product in products])
 
 
 def get_num_pages(soup) -> int:
-    count_pages = int(soup.select_one(".pagination .page-item:nth-last-of-type(2)").text)
+    count_pages = soup.select_one(".pagination .page-item:nth-last-of-type(2)")
     if count_pages is None:
         return 1
-
-    return count_pages
+    return int(count_pages.text)
 
 
 def parse_hdd_block_prices(product_soup: BeautifulSoup) -> dict[str, float]:
-    # get detalied url
-    # Driver get this page
-    # Driver find swatches
-    # Driver for each button click (if clickable) -> get price
     detailed_url = urljoin(URL_BASE, product_soup.select_one(".title")["href"])
-    driver = webdriver.Chrome()
+    driver = get_driver()
+    driver.get(detailed_url)
+    swatches = driver.find_element(By.CLASS_NAME, "swatches")
+    buttons = swatches.find_elements(By.TAG_NAME, "button")
+
+    prices = {}
+    for btn in buttons:
+        if not btn.get_property("disabled"):
+            btn.click()
+            prices[btn.get_property("value")] = float(driver.find_element(
+                By.CLASS_NAME, "price"
+            ).text.replace("$", ""))
+
+    return prices
+
+
 
 
 def parse_single_product(product_soup: BeautifulSoup) -> Product:
@@ -77,7 +96,7 @@ def parse_single_product(product_soup: BeautifulSoup) -> Product:
         price=float(product_soup.select_one(".price").text[1::]),
         rating=int(product_soup.select_one(".ratings p[data-rating]")["data-rating"]),
         reviews=int(product_soup.select_one(".ratings .pull-right").text.split()[0]),
-        additional_info={"hdd_prices": hdd_prices},
+        additional_info=hdd_prices,
     )
 
 
@@ -100,13 +119,16 @@ def get_laptop_products() -> [Product]:
         soup = BeautifulSoup(page, "html.parser")
 
         all_products.extend(get_single_page_products(soup))
+        break  # TODO: remove this - just for debugging
 
     return all_products
 
 
 def main():
-    products = get_laptop_products()
-    write_products_to_csv(products)
+    with webdriver.Chrome() as new_driver:
+        set_driver(new_driver)
+        products = get_laptop_products()
+        write_products_to_csv(products)
 
 
 if __name__ == '__main__':
