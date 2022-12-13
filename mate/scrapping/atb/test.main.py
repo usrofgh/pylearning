61cc095c2c5b2a040204b79be18ef5defda43b93
usrofgh <mailforgimail@gmail.com>
2022-12-13 13:58:31 +0200

    fixes

--- mate/scrapping/atb/test.main.py
+++ mate/scrapping/atb/test.main.py
@@ -1,31 +1,75 @@
-from dataclasses import astuple, dataclass
+import csv
+import json
+import logging
+import os
+import sys
+import time
+from dataclasses import dataclass, fields, astuple
 from urllib.parse import urljoin
-
-import bs4
 from bs4 import BeautifulSoup
 import requests
 
 
 HOME_PAGE = "https://www.atbmarket.com/"
+
 headers = {  # TODO: add user-agent lib changer
     "Accept": "*/*",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
                   "Chrome/108.0.0.0 Safari/537.36"
 }
 
+
+
+@dataclass
 class ATBProduct:
+    _code: int
+    _in_stock: str
+    _category: str
+    _title: str
+    _original_price: float
+    _discount_price: float
+    _discount_amount: str
+    _discount_date: str
+    _rating: str
+    _description: str
+    _characteristics: dict
+    _comments: list
+    
     def __init__(self, page):
-        self._set_title(page)
         self._set_code(page)
-        self._set_original_price(page)
-        self._set_discount_price(page)
-        self._set_discount_info(page)
         self._set_in_stock(page)
+        self._set_category(page)
+        self._set_title(page)
+        self._set_price(page)
+        self._set_discount_amount(page)
+        self._set_discount_date(page)
         self._set_rating(page)
-        self._set_brand(page)
-        self._set_country(page)
         self._set_description(page)
+        self._set_characteristic(page)
         self._set_comments(page)
+
+    def __str__(self):
+        print("category:", self.get_category())
+        print("title:", self.get_title())
+        print("code:", self.get_code())
+        print("original price:", self.get_original_price())
+        print("discount price:", self.get_discount_price())
+        print("discount_info:", self.get_discount_date())
+        print("discount_info:", self.get_discount_amount())
+        print("stock status:", self.get_stock_status())
+        print("rating:", self.get_rating())
+        print("description:", self.get_description())
+        print("comments:", self.get_comments())
+        return ""
+
+    def _set_category(self, page):
+        self._category = page.select_one(".breadcrumbs__item:nth-last-child(2) a")["href"].split("/")[-1].partition("-")[-1]
+
+    def _set_characteristic(self, page):
+        names = [name.text.strip() for name in page.select(".product-characteristics__row .product-characteristics__name")]
+        values = [value.text.strip() for value in page.select(".product-characteristics__row .product-characteristics__value")]
+        self._characteristics = dict(zip(names, values))
+        
 
     def _set_title(self, page):
         self._title = page.select_one(".page-title.product-page__title").text
@@ -33,59 +77,85 @@ class ATBProduct:
     def _set_code(self, page):
         self._code = page.select_one(".custom-tag__text strong").text
 
-    def _set_original_price(self, page):
-        self._original_price = page.select_one(".product-main .product-price__top").text.strip().split()[0]
-
-    def _set_discount_price(self, page):
+    def _set_price(self, page):
         try:
-            self._discount_price = page.select_one(".product-main .product-price__bottom").text
-        except:
+            prices = page.select_one(".product-main .product-price--sale").text
+            self._original_price = prices.select_one(".product-price__bottom").text.strip().split()[0]
+            self._discount_price = prices.select_one(".product-price__top").text.strip().split()[0]
+        except AttributeError:
+            self._original_price = page.select_one(".product-main .product-price__top").text.strip().split()[0]
             self._discount_price = None
 
-    def _set_discount_info(self, page):
-        interval_discount = page.select_one(".product-about__dates span:nth-child(2)")
-        if interval_discount is None:
-            self._discount_info = None
+
+
+    def _set_discount_amount(self, page):
+        try:
+            temp = page.select_one(".product-about__labels .custom-product-label").text
+        except AttributeError:
+            self._discount_amount = None
             return
-        start = interval_discount.split("-")[0].strip()
-        stop = interval_discount.split("-")[1].strip()
-        self._discount_info = {"date": {"start": start, "end": stop}}
+        if "-" in temp:
+            self._discount_amount = temp[1::]
+        else:
+            self._discount_amount = None
+
+
+    def _set_discount_date(self, page):
+        try:
+            interval_discount = page.select_one(".product-about__dates span:nth-child(2)").text
+        except AttributeError:
+            self._discount_date = None
+            return
+
+        interval_discount = interval_discount.split(" – ")
+        start = interval_discount[0]
+        stop = interval_discount[1]
+        self._discount_date = {"start": start, "end": stop}
 
     def _set_in_stock(self, page):
-        self._in_stock = bool(page.select_one(".available-tag.available-tag--grey"))
+        if page.select_one(".product-main .available-tag--grey"):
+            self._in_stock = "no"
+        elif page.select_one(".product-main .available-tag--orange"):
+            self._in_stock = "ends"
+        elif page.select_one(".product-main .product-about__available"):
+            self._in_stock = "yes"
 
     def _set_rating(self, page):
         self._rating = page.select_one(".product-main .rating__value").text
 
-    def _set_brand(self, page):
-        self._brand = page.select("#productCharacteristics "
-                                  ".product-characteristics__value")[1].text
-
-    def _set_country(self, page):
-        self._country = page.select("#productCharacteristics .product-characteristics__value")[0].text
-
     def _set_description(self, page):
-        desc = page.select_one(".product-characteristics__desc p")
-        if desc is not None:
-            self._desc = desc.text
-        else:
-            self._desc = None
+        try:
+            self._description = page.select_one(".product-characteristics__desc p").text
+        except AttributeError:
+            self._description = None
 
     def _set_comments(self, page):
         comments_list = page.select(".reviews-item__main")
         comments = []
-        if len(comments) == 0:
+        if len(comments_list) == 0:
             self._comments = None
             return
-        for comment in comments_list:
-            name = comment.select_one(".reviews-item__info .reviews-item__name")
-            date = comment.select_one(".reviews-item__time")
-            rating = comment.select_one(".rating")
 
-            comments.append({"name": name,
-                             "date": date,
-                             "rating": rating})
-            self._comments = comment
+        for comment in comments_list:
+            name = comment.select_one(".reviews-item__info .reviews-item__name").text
+            date = comment.select_one(".reviews-item__time").text
+            date_temp = date.split(".")
+            date = f"{date_temp[0]}.{date_temp[1]}.{date_temp[-1]}"
+            try:
+                rating = comment.select_one(".rating input:checked")["id"].split("-")[-1]
+            except TypeError:
+                rating = None
+            text_comment = comment.select_one(".reviews-item__desc").text
+
+
+            comments.append({"date": date,
+                             "name": name,
+                             "rating": rating,
+                             "text": text_comment.strip()})
+        self._comments = comments
+
+    def get_category(self):
+        return self._category
 
     def get_title(self):
         return self._title
@@ -99,8 +169,11 @@ class ATBProduct:
     def get_discount_price(self):
         return self._discount_price
 
-    def get_discount_info(self):
-        return self._discount_info
+    def get_discount_date(self):
+        return self._discount_date
+
+    def get_discount_amount(self):
+        return self._discount_amount
 
     def get_stock_status(self):
         return self._in_stock
@@ -108,83 +181,70 @@ class ATBProduct:
     def get_rating(self):
         return self._rating
 
-    def get_brand(self):
-        return self._brand
-
-    def get_country(self):
-        return self._country
-
     def get_description(self):
-        return self._desc
+        return self._description
 
     def get_comments(self):
         return self._comments
 
-
-def reading(path: str):
-    with open(path, "r", encoding="UTF-8") as file:
-        return file.read()
-
-
-def writing(path: str, type_writing: str, item):
-    with open(path, type_writing, encoding="UTF-8") as file:
-        file.write(item)
-
-# TODO: ...
-# home_page = requests.get(HOME_PAGE, headers).text
-# writing(item=home_page, add_write="w", path="data/index.html")
-
-# categories_pages = []
-#
-# page = reading("data/index.html")
-# home_page_soup = bs4.BeautifulSoup(page, "html.parser")
-# categories_links = [urljoin(HOME_PAGE, i["href"])
+# with open("data/index.html", "r", encoding="UTF-8") as file:
+#     page = file.read()
+# home_page_soup = BeautifulSoup(page, "html.parser")
+# categories_base_links = [urljoin(HOME_PAGE, i["href"])
 #                     for i in home_page_soup.select(".category-menu a.category-menu__link-wrap")[3::]]
+#
+# for category_link in categories_base_links:
+#     category_page = requests.get(category_link,
+#                                  params={"page": 500}, headers=headers).content
+#     category_page_soup = BeautifulSoup(category_page, "html.parser")
+#     try:
+#         count_page = category_page_soup.select_one(".product-pagination__list .active").text
+#     except AttributeError:
+#         count_page = 1
+#
+#     category_links = []
+#     for n_page in range(1, int(count_page) + 1):
+#         category_links.append(urljoin(category_link, f"?page={n_page}"))
+#
+#     for lnk in category_links:
+#         print(logging.INFO)
+#         with open("data/categories_links.txt", "a", encoding="UTF-8") as file:
+#             file.write(lnk)
+#             file.write("\n")
 
-# for category_link in categories_links[1:2]:  # TODO: delete [0:1]
-    # TODO:
-    # category_page = requests.get(category_link,
-    #                              params={"page": 500}, headers=headers).text
-    # writing(category_page, "a", "data/category_page.html")
 
 
-    # category_page = reading("data/category_page.html")
-    # category_page_soup = bs4.BeautifulSoup(category_page, "html.parser")
-    # count_page = category_page_soup.select_one(".product-pagination__list .active").text
-    #
-    # if count_page is None:
-    #     count_page = 1
-    # TODO: GENERATING PAGES
-    # for n_page in range(1, int(count_page) + 1):
-    #     categories_pages.append(urljoin(category_link, f"?page={n_page}"))
-    # TODO: WRITING GENERATED PAGES
-    # with open("data/categories_pages.txt", "a", encoding="UTF-8") as f:
-    #     my_iter = iter(categories_pages)
-    #     f.write(next(my_iter, ''))
-    #     for line in my_iter:
-    #         f.write('\n')
-    #         f.write(line)
+# with open("data/categories_links.txt", "r", encoding="UTF-8") as file:
+#     generated_category_links = file.read().split("\n")[:-1]
+# category_links = []
+# for category_link in generated_category_links:
+#     page = requests.get(category_link, headers=headers).content  # TODO: do it with scrapy/asyncio
+#     print("request")
+#     page_soup = BeautifulSoup(page, "html.parser")
+#     category_links.extend([
+#         urljoin(HOME_PAGE, item["href"])
+#         for item in page_soup.select(".catalog-list .catalog-item__title a")
+#     ])
+#
+# with open("data/all_products_links.txt", "a", encoding="UTF-8") as file:
+#     for link in category_links:
+#         file.write(link)
+#         file.write("\n")
 
-# generated_category_pages = reading("data/categories_pages.txt")
-# page = generated_category_pages.split("\n")[0]
-# page_soup = BeautifulSoup(page, "html.parser")
+with open("data/all_products_links.txt", "r", encoding="UTF-8") as file:
+    all_roduct_links = file.read().split("\n")[:-1]
 
-# category_page_soup = bs4.BeautifulSoup(reading("data/category_page.html"), "html.parser")
-# links = [urljoin(HOME_PAGE, item["href"])
-#          for item in category_page_soup.select(".catalog-list .catalog-item__title a")]
-# product = requests.get(links[0], headers=headers).text
-# writing(item=product, type_writing="w", path="data/product.html")
+# atb_product_json = json.dumps(atb_product.__dict__, ensure_ascii=False, indent=2)
+json_products = []
 
-page = BeautifulSoup(reading("data/product.html"), "html.parser")
-atb_product = ATBProduct(page)
-print(atb_product.get_title())
-print(atb_product.get_code())
-print(atb_product.get_original_price())
-print(atb_product.get_discount_price())
-print(atb_product.get_discount_info())
-print(atb_product.get_stock_status())
-print(atb_product.get_rating())
-print(atb_product.get_brand())
-print(atb_product.get_country())
-print(atb_product.get_description())
-print(atb_product.get_comments())
+with open("data/product_test.csv", "a", encoding="UTF-8", newline="") as file:
+    csv_headers = [field.name for field in fields(ATBProduct)]
+    writer = csv.writer(file)
+    writer.writerow(csv_headers)
+    s = time.perf_counter()
+    for link in all_roduct_links:
+        page_soup = BeautifulSoup(requests.get(link, headers=headers).content, "html.parser")
+        atb_product = ATBProduct(page_soup)
+        writer.writerow(astuple(atb_product))
+    e = time.perf_counter()
+    print(f"Elapsed: {round(e - s, 2)} sec.")  # Elapsed: 12.43 sec. for 10 requests
