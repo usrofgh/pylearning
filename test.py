829c17a023f14419e595188cef4acc5c16282c2c
usrofgh <mailforgimail@gmail.com>
2023-02-06 13:25:17 +0200

    update

--- test.py
+++ test.py
@@ -1,8 +1,39 @@
-a = 3
+class Wallet:
+    def __init__(self, brand: str, money_inside: int):
+        self.brand = brand
+        self.money_inside = money_inside
 
-def change_a():
-    a = 1
-    global a
-    a = 5
+class User:
+    def __init__(
+            self,
+            name: str,
+            age: int,
+            wallet: Wallet = None,
+            best_friend = None
+    ):
+        self.name = name
+        self.age = age
+        self.wallet = wallet  # composition
+        self.best_friend = best_friend
 
-change_a()
+katia = User("Katia", 26)
+john = User("John", 29)
+sonia = User("Sonia", 36)
+
+katia.best_friend = john
+john.best_friend = sonia
+sonia.best_friend = katia
+
+katia.wallet = Wallet("Adidas", 0)
+
+katia.best_friend.wallet = katia.wallet
+katia.wallet = None
+
+katia.best_friend.best_friend.wallet = katia.best_friend.wallet
+katia.best_friend.wallet = None
+
+katia.best_friend.best_friend.best_friend.wallet = katia.best_friend.best_friend.wallet
+katia.best_friend.best_friend.wallet = None
+
+
+print(katia.wallet.brand)
